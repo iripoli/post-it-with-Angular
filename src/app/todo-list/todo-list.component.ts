3f49fc39bd670f7d8ef58d55b123ea114972efb7
iripoli <iripoli96@gmail.com>
2020-02-03 18:19:43 +0100

    Maquetado de los posts y cambios en los estilos

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-list-cards',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
